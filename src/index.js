1ad3d4997c1331e60dbf04c24b8ffc7a336c3ad2
Rohit-del-source <rohitverma18158920@gmail.com>
2024-02-23 09:12:08 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  try {
+    // Write the content to the file
+    await fs.writeFile(fileName, fileContent);
+    console.log(`File ${fileName} created and data written successfully!`);
+  } catch (error) {
+    console.error(`Error writing to ${fileName}:`, error);
+  }
 };
 
 module.exports = { writeFile };
